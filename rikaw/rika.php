f36ccac914448473b55ed83afe59174e36ed26df
bayujunisp <bayupribadi37@gmail.com>
2024-01-19 18:08:50 +0700

    rikawidiyanti

---
+++ rikaw/rika.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>Happy Birthday!!! :)</title>
+  <!-- Google Font -->
+  <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400" rel="stylesheet" />
+  <link rel="shortcut icon" type="image/png" href="img/favicon.png" />
+  <link rel="stylesheet" href="styles/main.css" />
+</head>
+
+<body>
+
+<!-- paste your html in here -->
+
+</body>
+
+<audio class="song" loop autoplay>
+  <source src="./music/hbd.mpeg">
+  </source>
+  Your browser isn't invited for super fun audio time.
+</audio>
+
+<div class="container">
+  <div class="one">
+    <h1 class="one">
+      
+     <!--  <span id="name"></span> -->
+    </h1>
+    <p class="two" id="greetingText">For The Most Adorable, Most Beautiful, Cutest, Strongest, Sweetest Girl IN THE WORLD</p>
+  </div>
+
+  <div class="three">
+    <p>Hari ini kan hari ulang tahun kamu yak :D</p>
+  </div>
+
+  <div class="four">
+    <div class="text-box">
+      <p class="hbd-chatbox">
+        Selamat Ulang Tahun yaa!!  
+      </p>
+      <p class="fake-btn">Send</p>
+    </div>
+  </div>
+
+  <div class="five">
+    <p class="idea-1">Itu yang mau aku lakukan, tadinya...</p>
+    <p class="idea-2">Tapi aku berubah pikiran.</p>
+    <p class="idea-3">
+      Aku sadar, Bahwa aku ingin melakukan sesuatu yang<br>
+      <strong>special</strong>
+      .
+    </p>
+    <p class="idea-4">Karena ,</p>
+    <p class="idea-5">
+      Kamu itu Special
+      <span>:)</span>
+    </p>
+    <p class="idea-6">
+      <span>Jadi</span>
+    </p>
+  </div>
+
+  <div class="six">
+    <img src="./img/ayu.jpg" alt="profile" class="profile-picture" id="imagePath"/>
+    <img src="img/hat.svg" alt="hat" class="hat" />
+    <div class="wish">
+      <h3 class="wish-hbd">Happy Birthday!</h3>
+      <h5 id="wishText">May Allah SWT always be with you! :)<br><h5>
+    </div>
+  </div>
+
+  <div class="seven">
+    <div class="baloons">
+      <img src="img/ballon2.svg" alt="" />
+      <img src="img/ballon1.svg" alt="" />
+      <img src="img/ballon3.svg" alt="" />
+      <img src="img/ballon1.svg" alt="" />
+      <img src="img/ballon2.svg" alt="" />
+      <img src="img/ballon3.svg" alt="" />
+      <img src="img/ballon2.svg" alt="" />
+      <img src="img/ballon3.svg" alt="" />
+      <img src="img/ballon1.svg" alt="" />
+      <img src="img/ballon2.svg" alt="" />
+      <img src="img/ballon3.svg" alt="" />
+      <img src="img/ballon2.svg" alt="" />
+      <img src="img/ballon1.svg" alt="" />
+      <img src="img/ballon3.svg" alt="" />
+      <img src="img/ballon2.svg" alt="" />
+      <img src="img/ballon3.svg" alt="" />
+      <img src="img/ballon1.svg" alt="" />
+      <img src="img/ballon2.svg" alt="" />
+      <img src="img/ballon1.svg" alt="" />
+      <img src="img/ballon3.svg" alt="" />
+      <img src="img/ballon3.svg" alt="" />
+      <img src="img/ballon1.svg" alt="" />
+      <img src="img/ballon2.svg" alt="" />
+      <img src="img/ballon3.svg" alt="" />
+      <img src="img/ballon2.svg" alt="" />
+      <img src="img/ballon1.svg" alt="" />
+      <img src="img/ballon3.svg" alt="" />
+      <img src="img/ballon2.svg" alt="" />
+      <img src="img/ballon3.svg" alt="" />
+      <img src="img/ballon1.svg" alt="" />
+      <img src="img/ballon2.svg" alt="" />
+      <img src="img/ballon1.svg" alt="" />
+      <img src="img/ballon3.svg" alt="" />
+    </div>
+  </div>
+
+  <div class="eight">
+    <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="20" cy="20" r="20" />
+    </svg>
+    <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="20" cy="20" r="20" />
+    </svg>
+    <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="20" cy="20" r="20" />
+    </svg>
+    <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="20" cy="20" r="20" />
+    </svg>
+    <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="20" cy="20" r="20" />
+    </svg>
+    <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="20" cy="20" r="20" />
+    </svg>
+    <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="20" cy="20" r="20" />
+    </svg>
+    <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="20" cy="20" r="20" />
+    </svg>
+    <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="20" cy="20" r="20" />
+    </svg>
+  </div>
+
+  <div class="nine">
+    <p>Sekian, semoga kamu suka :D</p>
+    <p>Doaku untuk kamu Semoga semakin menjadi orang yang lebih baik lagi, panjang umur, melimpah rezeki, dan 
+        diberikan kesehatan selalu</p>
+    <p>Semangat selalu dalam meraih mimpi dan di jalan yang kamu ambil</p>
+    <p>Salam juga untuk keluarga kamu dirumah</p>
+   <!--  <p>Okay, now come back and tell me if you liked it.</p> -->
+    <p id="replay"><b>Click Here, if you want to watch it again.</b></p>
+    <p class="last-smile">:)</p>
+  </div>
+</div>
+
+  <!-- Greensock -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js"></script>
+  <!-- Sweetalert -->
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <script type="application/javascript" src="script/main.js"></script>
+
+</html>
